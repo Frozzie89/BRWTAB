02a3c195930ff62945b79feaf0bf1d775ed562cf
Frozzie89 <froztor85@gmail.com>
2025-12-30 20:35:10 +0100

    Implement new group form with validation and styling

--- client/src/app/components/new-group/new-group.component.ts
+++ client/src/app/components/new-group/new-group.component.ts
@@ -1,13 +1,22 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { ListGroupItem } from '../../interfaces/list-group-item';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-group',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './new-group.component.html',
   styleUrl: './new-group.component.scss',
 })
 export class NewGroupComponent {
   @Output() cancel = new EventEmitter<void>();
   @Output() create = new EventEmitter<ListGroupItem>();
+
+  groupName: string = '';
+  groupDescription: string = '';
+
+  createGroup() {
+    throw new Error('Method not implemented.');
+  }
 }
