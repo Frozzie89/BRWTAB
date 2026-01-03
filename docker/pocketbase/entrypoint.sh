56a14f29fba211568be48d865eb24facd6b65c45
Frozzie89 <froztor85@gmail.com>
2026-01-03 16:54:34 +0100

    removed unecessary entrypoint sh script since pocketbase automigrates by default

--- docker/pocketbase/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Running migrations..."
-pocketbase migrate up --dir /pb/pb_data
-
-echo "Starting PocketBase..."
-exec pocketbase serve --http=0.0.0.0:8090 --dir /pb/pb_data
