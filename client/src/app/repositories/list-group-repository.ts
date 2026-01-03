e6cc7d3e8862f7d6436268a6d5049fb35172252f
Frozzie89 <froztor85@gmail.com>
2026-01-03 13:23:21 +0100

    new Groups repository

---
+++ client/src/app/repositories/list-group-repository.ts
@@ -0,0 +1,66 @@
+import { Injectable, inject } from '@angular/core';
+import { PocketbaseClientService } from '../services/pocketbase-client.service';
+import type { Group, CreateGroupPayload } from '../interfaces/group';
+
+export type PbGroupRecord = {
+  id: string;
+  title: string;
+  description: string;
+  color: string;
+  icon?: string;
+  created: string;
+  updated: string;
+};
+
+@Injectable({ providedIn: 'root' })
+export class GroupsRepository {
+  private readonly pb = inject(PocketbaseClientService).pb;
+  private readonly collection = 'groups';
+
+  async list(): Promise<Group[]> {
+    const records = await this.pb.collection(this.collection).getFullList<PbGroupRecord>({
+      sort: '-created',
+    });
+    return records.map(r => this.toGroup(r));
+  }
+
+  async create(data: CreateGroupPayload): Promise<Group> {
+    const fd = new FormData();
+    fd.append('title', data.title);
+    fd.append('description', data.description);
+    fd.append('color', data.color);
+
+    if (data.iconFile) {
+      fd.append('icon', data.iconFile);
+    }
+
+    const created = await this.pb.collection(this.collection).create<PbGroupRecord>(fd);
+    return this.toGroup(created);
+  }
+
+  delete(id: string): Promise<boolean> {
+    return this.pb.collection(this.collection).delete(id);
+  }
+
+  subscribeAll(onEvent: (e: any) => void) {
+    return this.pb.collection(this.collection).subscribe('*', onEvent);
+  }
+
+  unsubscribeAll() {
+    return this.pb.collection(this.collection).unsubscribe('*');
+  }
+
+  toGroupFromRecord(record: PbGroupRecord): Group {
+    return this['toGroup'](record as any);
+  }
+
+  toGroup(r: PbGroupRecord): Group {
+    return {
+      id: r.id,
+      title: r.title,
+      description: r.description,
+      color: r.color,
+      iconUrl: r.icon ? this.pb.files.getUrl(r as any, r.icon) : null,
+    };
+  }
+}
