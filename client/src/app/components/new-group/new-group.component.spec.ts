db3ed63b386f41722b08c54c4b69458feace3dd7
Frozzie89 <froztor85@gmail.com>
2026-01-06 23:44:46 +0100

    test: add stubs for ActivatedRoute and GroupEntriesService in NewGroupComponent tests

--- client/src/app/components/new-group/new-group.component.spec.ts
+++ client/src/app/components/new-group/new-group.component.spec.ts
@@ -1,14 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewGroupComponent } from './new-group.component';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { GroupEntriesService } from '../../services/group-entries.service';
 
 describe('NewGroupComponent', () => {
   let component: NewGroupComponent;
   let fixture: ComponentFixture<NewGroupComponent>;
 
   beforeEach(async () => {
+    const activatedRouteStub = {
+      data: of({ group: { id: 'test-id', name: 'Test Group' } }),
+    };
+
+    const groupEntriesServiceStub = {
+      groupsEntries: () => [],
+      loadByGroupId: jasmine.createSpy('loadByGroupId'),
+      enableRealtime: jasmine.createSpy('enableRealtime'),
+      add: jasmine.createSpy('add'),
+    };
+
     await TestBed.configureTestingModule({
       imports: [NewGroupComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteStub },
+        { provide: GroupEntriesService, useValue: groupEntriesServiceStub },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NewGroupComponent);
